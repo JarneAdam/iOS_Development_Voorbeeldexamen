437d20cf1dc908b9f78afb416188d78fb3de7ff1
JarneAdam <jarne.adam3@gmail.com>
2025-12-17 09:05:48 +0100

    Added files from teacher

---
+++ Voorbeeldexamen/Voorbeeldexamen/VoorbeeldexamenApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoorbeeldexamenApp.swift
+//  Voorbeeldexamen
+//
+//  Created by Jarne Adam on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct VoorbeeldexamenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
